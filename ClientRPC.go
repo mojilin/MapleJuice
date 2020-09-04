909ae89b37978c26c4a012ce733d920a177740f8
mojilin <linxiaoxinleo@gmail.com>
2020-09-04 12:25:45 -0500

    add lines to trigger CI

--- ClientRPC.go
+++ ClientRPC.go
@@ -14,6 +14,7 @@ type Client struct {
 	Alias      string
 	rpcClients map[string]*rpc.Client //stores RPC connection from nodeIP -> rpc.clients
 }
+
 //------------------------------HELP FUNCTIONS--------------------------------
 func initClient(selfNode Node) *Client {
 	return &Client{IP: selfNode.IP, Alias: selfNode.Alias, rpcClients: map[string]*rpc.Client{}}
@@ -24,9 +25,9 @@ func (c *Client) Connect(nodeIP string) (*rpc.Client, error) {
 	_, exists := c.rpcClients[nodeIP]
 
 	if exists == false {
-		client, err := rpc.Dial("tcp", nodeIP +RpcPort)
+		client, err := rpc.Dial("tcp", nodeIP+RpcPort)
 		if err != nil {
-			errHandler(err, false, "Error when dialing rpc to" + getAlias(nodeIP))
+			errHandler(err, false, "Error when dialing rpc to"+getAlias(nodeIP))
 			return nil, err
 		}
 		printAndLogTime("RPC tcp connection established from" + selfNode.Alias + "to" + getAlias(nodeIP))
@@ -48,35 +49,35 @@ func (c *Client) uploadFile(fileName string, timeStamp time.Time, src string, se
 	// connect to server
 	client, _ := c.Connect(serverIP)
 	if err := client.Call("Rpc.ReceiveFile", ReceiveReq{
-		FromNode: selfNode,
-		FileName: fileName,
-		FileSize: getFileSize(fileName),
-		Dst:      "./data/",
-		Dir2: dir2,
+		FromNode:  selfNode,
+		FileName:  fileName,
+		FileSize:  getFileSize(fileName),
+		Dst:       "./data/",
+		Dir2:      dir2,
 		TimeStamp: timeStamp,
 	}, &res); err != nil {
 		errHandler(err, false, "Error when calling Rpc.ReceiveFile on "+serverIP)
 		return err
 	}
-	conn, err := net.Dial("tcp", serverIP +DownloadPort)
-	errHandler(err, false, "Error when dialing tcp to "+ serverIP)
+	conn, err := net.Dial("tcp", serverIP+DownloadPort)
+	errHandler(err, false, "Error when dialing tcp to "+serverIP)
 	defer conn.Close()
 
 	// open file to upload
 	//mux.Lock()
 	//defer mux.Unlock()
 
-	printAndLogTime("Uploading " + strconv.Itoa(hash([]byte(fileName)))+ " to "+ getAlias(serverIP) + "....")
+	printAndLogTime("Uploading " + strconv.Itoa(hash([]byte(fileName))) + " to " + getAlias(serverIP) + "....")
 	f, err := os.Open(src + fileName)
-	errHandler(err, false, "Error when opening file" + src+fileName)
+	errHandler(err, false, "Error when opening file"+src+fileName)
 	defer f.Close()
 	info, err := f.Stat()
 
 	// upload
 	_, err = io.Copy(conn, f)
-	errHandler(err, false, "Error when upload io copying file :" + src+fileName)
-	if err == nil{
-		printAndLogTime("Upload complete! Size: " + strconv.Itoa(int(info.Size()))+ "   ")
+	errHandler(err, false, "Error when upload io copying file :"+src+fileName)
+	if err == nil {
+		printAndLogTime("Upload complete! Size: " + strconv.Itoa(int(info.Size())) + "   ")
 	}
 
 	return err
@@ -88,14 +89,13 @@ func (c *Client) downloadFile(fileName string, src string, dstFileName string, s
 
 	// connect to server
 	client, _ := c.Connect(serverIP)
-	if err := client.Call("Rpc.SendFile", SendReq{FromNode: selfNode, FileName:fileName, Src:src, Dir2: dir2}, &res); err != nil {
+	if err := client.Call("Rpc.SendFile", SendReq{FromNode: selfNode, FileName: fileName, Src: src, Dir2: dir2}, &res); err != nil {
 		errHandler(err, false, "Error when calling Rpc.SendFile on "+serverIP)
 		return err
 	}
-	conn, err := net.Dial("tcp", serverIP +DownloadPort)
-	errHandler(err, false, "Error when dialing tcp to "+ serverIP)
+	conn, err := net.Dial("tcp", serverIP+DownloadPort)
+	errHandler(err, false, "Error when dialing tcp to "+serverIP)
 	defer conn.Close()
-
 
 	dir2 = res.Dir2
 	// open new file to write to
@@ -103,13 +103,13 @@ func (c *Client) downloadFile(fileName string, src string, dstFileName string, s
 	//defer mux.Unlock()
 
 	f, err := os.OpenFile(dstFileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	errHandler(err, false, "Error when opening new file" + dstFileName)
+	errHandler(err, false, "Error when opening new file"+dstFileName)
 	defer f.Close()
 
 	// download
-	printAndLogTime("Downloading file from "+ getAlias(serverIP) + "....")
+	printAndLogTime("Downloading file from " + getAlias(serverIP) + "....")
 	_, err = io.Copy(f, conn)
-	errHandler(err, false, "Error when download io copying file :" + dstFileName)
+	errHandler(err, false, "Error when download io copying file :"+dstFileName)
 
 	//s, _ := f.Stat()
 	//if s.Size() != res.FileSize {
@@ -117,10 +117,9 @@ func (c *Client) downloadFile(fileName string, src string, dstFileName string, s
 	//	err = errors.New("FileSize not equal!")
 	//}
 
-	if err == nil{
+	if err == nil {
 		info, _ := f.Stat()
-		printAndLogTime("Download complete! Size : " + strconv.Itoa(int(info.Size()))+ "   ")
-
+		printAndLogTime("Download complete! Size : " + strconv.Itoa(int(info.Size())) + "   ")
 
 		if dstFileName[:7] == "./data/" {
 			removeFile(&MySDFSFiles, hash([]byte(fileName)))
@@ -128,12 +127,12 @@ func (c *Client) downloadFile(fileName string, src string, dstFileName string, s
 				FileHash:  hash([]byte(fileName)),
 				FileName:  fileName,
 				TimeStamp: res.TimeStamp,
-				Dir2: dir2,
+				Dir2:      dir2,
 			})
 		}
 	}
 
-	printAndLogTime("file: "+fileName+ " received!")
+	printAndLogTime("file: " + fileName + " received!")
 
 	return err
 }
@@ -145,12 +144,13 @@ func (c *Client) updateDHT(node Node) error {
 	client, _ := c.Connect(node.IP)
 	if err := client.Call("Rpc.UpdateDHT", DHTReq{
 		//NodeHashKeys: NodeHashKeys,
-		AllFile:      AllFiles,
+		AllFile: AllFiles,
 	}, &res); err != nil {
 		return err
 	}
 	return nil
 }
+
 // file conflict, let user make choice
 func (c *Client) callPopWindow(ip string, fileName string) (error, bool) {
 	var res bool
@@ -160,6 +160,7 @@ func (c *Client) callPopWindow(ip string, fileName string) (error, bool) {
 	}
 	return nil, res
 }
+
 //------------------------------FOR CLIENT FUNCTIONS--------------------------------
 
 func (c *Client) putFile(fileName string, timeStamp time.Time, fromNodeIP string, toNodeIP string, src string, dir2 string) (error, bool) {
@@ -172,7 +173,7 @@ func (c *Client) putFile(fileName string, timeStamp time.Time, fromNodeIP string
 		FileName:  fileName,
 		TimeStamp: timeStamp,
 		Src:       src,
-		Dir2:dir2,
+		Dir2:      dir2,
 	}, &res); err != nil {
 		return err, false
 	}
@@ -188,7 +189,6 @@ func (c *Client) removeFileAll(fileName string, nodeIP string) error {
 	}
 	return nil
 }
-
 
 func (c *Client) removeFileAt(fileName string, nodeIP string) error {
 	var res bool
@@ -216,6 +216,7 @@ func (c *Client) sendTask(task Task, nodeIP string) error {
 	return nil
 
 }
+
 /*
  * rpc start maple
  * workType: Maple, Juice, MapleJuice
@@ -257,22 +258,19 @@ func (c *Client) getAllSDFSFiles() []File {
 	}
 
 	//drop duplicates
-		seen := make(map[string]struct{}, len(result))
-		j := 0
-		for _, file := range result {
-			if _, ok := seen[file.FileName]; ok {
+	seen := make(map[string]struct{}, len(result))
+	j := 0
+	for _, file := range result {
+		if _, ok := seen[file.FileName]; ok {
 			continue
 		}
 		seen[file.FileName] = struct{}{}
 		result[j] = file
 		j++
 	}
-		return result[:j]
+	return result[:j]
 
 }
-
-
-
 
 //func (c *Client) sendKeyMap(keyMap map[string][]string, nodeIP string) error {
 //	var res bool
@@ -292,11 +290,10 @@ func getFileSize(fileName string) int64 {
 
 	f, err := os.Open("./data/" + fileName)
 	defer f.Close()
-	errHandler(err, false, "Error when opening file: " + fileName)
+	errHandler(err, false, "Error when opening file: "+fileName)
 	if err == nil {
 		s, _ := f.Stat()
 		return s.Size()
 	}
-
 	return 0
 }
